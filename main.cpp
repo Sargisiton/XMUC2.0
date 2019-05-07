20362cd3f12c1106b9a9742b2d5eb41b8229833c
Sargisiton <49852096+Sargisiton@users.noreply.github.com>
2019-05-07 21:12:38 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<string>
+#include"class_libary.h"
+using namespace std;
+class PersonalChoiceUI;
+
+int main(void){
+    ofstream ofile ("c:\орн▒\codeblocks\XMUC++\information.txt",ios::out);
+    PersonalChoiceUI A;
+    A.show();
+
+
+
+
+system("pause");
+return 0;
+}
+
